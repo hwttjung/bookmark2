98c1c0f197d0cf335f233935da7e9b4d4df3eeab
Hewitt Jung <hwttjung@gmail.com>
2022-04-13 16:57:13 +0900

    second commit

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,30 @@
+from dataclasses import field
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
+
+from .models import Bookmark
+
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+
+class BookmarkupdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
