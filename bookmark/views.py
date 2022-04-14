f1a97b7b856c161549d54359c91a04173a858845
Hewitt Jung <hwttjung@gmail.com>
2022-04-14 16:56:52 +0900

    third commit

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 
 from .models import Bookmark
@@ -11,7 +11,7 @@ from .models import Bookmark
 
 class BookmarkListView(ListView):
     model = Bookmark
-
+    paginate_by = 4
 
 class BookmarkCreateView(CreateView):
     model = Bookmark
@@ -22,9 +22,15 @@ class BookmarkCreateView(CreateView):
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
+    
 
 
 class BookmarkupdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
+
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
